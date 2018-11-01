22fbc94411b82180e530b4a765ec72d738e3907a
Lê Trung Kiên <43262409+lawlietsnow@users.noreply.github.com>
2018-11-01 20:42:25 +0000

    Update Task2.java

--- src/week7/task2/Task2.java
+++ src/week7/task2/Task2.java
@@ -1,4 +1,62 @@
 package week7.task2;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Task2 {
+    public static void main(String[] args) {
+        try{
+            nullPointer();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        try{
+            outOfBound();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        try{
+            arithmeti();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        try{
+            cast();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        try{
+            iO();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        try{
+            fileNotFound();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+    public static void nullPointer() throws NullPointerException{
+        throw new NullPointerException();
+    }
+    public static void outOfBound() throws ArrayIndexOutOfBoundsException{
+        throw new ArrayIndexOutOfBoundsException();
+    }
+    public static void arithmeti() throws ArithmeticException{
+        throw new ArithmeticException();
+    }
+    public static void cast() throws ClassCastException{
+        throw new ClassCastException();
+    }
+    public static void iO() throws IOException{
+        throw new IOException();
+    }
+    public static void fileNotFound() throws FileNotFoundException{
+        throw new FileNotFoundException();
+    }
 }
+
+    
+
+
+
