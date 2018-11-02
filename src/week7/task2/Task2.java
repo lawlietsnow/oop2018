f553c31ad21b24c316c32f015c643268023fc2f7
thotang19@gmail.com <thotang19@gmail.com>
2018-11-02 16:37:09 +0700

    task 2

--- src/week7/task2/Task2.java
+++ src/week7/task2/Task2.java
@@ -1,6 +1,9 @@
 package week7.task2;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class Task2 {
@@ -36,23 +39,32 @@ public class Task2 {
             System.out.println(e);
         }
     }
-    public static void nullPointer() throws NullPointerException{
-        throw new NullPointerException();
+    public static int nullPointer() throws NullPointerException{
+        int a[] = null;
+        return a[33];
     }
-    public static void outOfBound() throws ArrayIndexOutOfBoundsException{
-        throw new ArrayIndexOutOfBoundsException();
+    public static int outOfBound() throws ArrayIndexOutOfBoundsException{
+        int a[] = {1,1};
+        return a[2];
     }
     public static void arithmeti() throws ArithmeticException{
-        throw new ArithmeticException();
+        //throw new ArithmeticException();
+        System.out.println(3/0);
     }
     public static void cast() throws ClassCastException{
-        throw new ClassCastException();
+        //throw new ClassCastException();
+        Object a = new Integer(6);
+        String s = (String) a;
     }
     public static void iO() throws IOException{
-        throw new IOException();
+        //throw new IOException();
+        File file=new File("fail.txt");
+        BufferedReader bt= new BufferedReader(new FileReader(file));
     }
     public static void fileNotFound() throws FileNotFoundException{
-        throw new FileNotFoundException();
+        //throw new FileNotFoundException();
+        File file=new File("fail.txt");
+        BufferedReader bt= new BufferedReader(new FileReader(file));
     }
 }
 
