c2a46364b3a9fd158519b0bbb6e7b394ffad706d
Lê Trung Kiên <43262409+lawlietsnow@users.noreply.github.com>
2018-11-02 14:46:14 +0700

    Update Addition.java

--- src/week7/task1/Addition.java
+++ src/week7/task1/Addition.java
@@ -1,4 +1,30 @@
 package week7.task1;
 
-public class Addition {
+public class Addition extends BinaryExpression{
+    private Expression left;
+    private Expression right;
+    
+    @Override
+    public Expression left() {
+        return left;
+    }
+
+    @Override
+    public Expression right() {
+        return right;
+    }
+
+    @Override
+    public String toString() {
+        return ""+left.toString()+"+"+right.toString();
+    }
+
+    @Override
+    public int evaluate() {
+         return left.evaluate()+right.evaluate();
+    }
+    Addition(Expression left,Expression right){
+        this.left=left;
+        this.right=right;
+    }
 }
