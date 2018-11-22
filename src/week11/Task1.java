f93bf26cb904fbe3a04760fa0f9b9ceda37e5545
lawlietsnow <thotang19@gmail.com>
2018-11-23 03:40:05 +0700

    asd

--- src/week11/Task1.java
+++ src/week11/Task1.java
@@ -1,4 +1,26 @@
 package week11;
 
-public class Task1 {
+public class Task1  {
+    public <T> void swap(T[]arr,int i,int j){
+        
+    }
+    public static <T extends Comparable> void sort(T[] arr){
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length;j++){
+                if(arr[i].compareTo(arr[j])<0) {
+                    T temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        Integer[] b = {1, 2, 3,5,4};
+        sort(b);
+        for(int i=0;i<b.length;i++) System.out.println(b[i]);
+        
+    }
+     
 }
--- src/week11/Task2.java
+++ src/week11/Task2.java
@@ -1,4 +1,27 @@
 package week11;
 
+import java.util.ArrayList;
+
 public class Task2 {
+    public static <T extends Comparable > T max(ArrayList<T> list){
+        T max=list.get(0);
+        for(int i=1;i<list.size();i++){
+            if(max.compareTo(list.get(i))<0) max=list.get(i);
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        ArrayList<Integer> list=new ArrayList<Integer>();
+        list.add(new Integer(0));
+        list.add(new Integer(0));
+        list.add(new Integer(0));
+        list.add(new Integer(0));
+        list.add(new Integer(0));
+        list.add(new Integer(0));
+        list.add(new Integer(0));
+        int a=max(list);
+        System.out.println(a);
+        
+    }
+    
 }
